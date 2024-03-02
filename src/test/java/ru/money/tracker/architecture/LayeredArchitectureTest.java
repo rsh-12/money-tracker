17857f3e215dc4258a9fb75b48f46f71228aede8
Shukhrat Rakhmonov <get_info@bk.ru>
2024-03-02 13:42:13 +0500

    Update archi-tests

--- src/test/java/ru/money/tracker/architecture/LayeredArchitectureTest.java
+++ src/test/java/ru/money/tracker/architecture/LayeredArchitectureTest.java
@@ -3,7 +3,11 @@ package ru.money.tracker.architecture;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 @AnalyzeClasses(packages = "ru.money.tracker")
@@ -24,4 +28,26 @@ public class LayeredArchitectureTest {
             .whereLayer("Controller").mayNotBeAccessedByAnyLayer()
             .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller")
             .whereLayer("Repository").mayOnlyBeAccessedByLayers("Service");
+
+    @ArchTest
+    static ArchRule controllers_should_be_suffixed_and_annotated =
+            classes()
+                    .that().resideInAPackage(CONTROLLER_PACKAGE + "..")
+                    .and().areAnnotatedWith(RestController.class)
+                    .should().haveSimpleNameEndingWith("Controller");
+
+    @ArchTest
+    static ArchRule services_should_be_suffixed_and_annotated =
+            classes()
+                    .that().resideInAPackage(SERVICE_PACKAGE + "..")
+                    .and().areAnnotatedWith(Service.class)
+                    .should().haveSimpleNameEndingWith("Service");
+
+    @ArchTest
+    static ArchRule repositories_should_be_suffixed_and_annotated =
+            classes()
+                    .that().resideInAPackage(REPOSITORY_PACKAGE + "..")
+                    .and().areAnnotatedWith(Repository.class)
+                    .should().haveSimpleNameEndingWith("Repository");
+
 }
