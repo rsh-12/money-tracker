4112205e19752f36e515e6223ff855363b4e1c05
Shukhrat Rakhmonov <get_info@bk.ru>
2024-05-16 00:04:37 +0500

    Update generate_ssl.sh
    skip-checks:true

--- config/certs/generate_crts.sh
+++ config/certs/generate_crts.sh
@@ -1,8 +1,8 @@
 # generate the private key and csr
-openssl req -new -newkey rsa:2048 -nodes -keyout backend.key -out backend.csr
+openssl req -new -newkey rsa:2048 -nodes -keyout "$(dirname "$0")"/backend.key -out "$(dirname "$0")"/backend.csr
 
 # generate crt with the key and the csr
-openssl x509 -req -days 36500 -in backend.csr -signkey backend.key -out backend.crt
+openssl x509 -req -days 36500 -in "$(dirname "$0")"/backend.csr -signkey "$(dirname "$0")"/backend.key -out "$(dirname "$0")"/backend.crt
 
 # convert crt to pem
-openssl x509 -in backend.crt -out backend.pem
+openssl x509 -in "$(dirname "$0")"/backend.crt -out "$(dirname "$0")"/backend.pem
