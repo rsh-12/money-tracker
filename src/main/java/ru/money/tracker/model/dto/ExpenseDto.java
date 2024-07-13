ad990bedec655f14e21a9f75b33a7da02c871986
Shukhrat Rakhmonov <get_info@bk.ru>
2024-07-13 10:17:48 +0500

    Update ExpenseDto

--- src/main/java/ru/money/tracker/model/dto/ExpenseDto.java
+++ src/main/java/ru/money/tracker/model/dto/ExpenseDto.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 
 @Getter
@@ -16,13 +17,8 @@ public class ExpenseDto {
     private Long id;
     private String title;
     private BigDecimal amount;
+    private LocalDate date;
     private String comment;
     private OffsetDateTime createdAt;
     private OffsetDateTime updatedAt;
-
-    public ExpenseDto(String title, BigDecimal amount, String comment) {
-        this.title = title;
-        this.amount = amount;
-        this.comment = comment;
-    }
 }
